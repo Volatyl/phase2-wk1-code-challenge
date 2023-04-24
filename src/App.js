0022aee0414a93cdb66cfa6a866a23593ac4a3f1
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-24 14:34:23 +0300

    Adding a new transaction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((r) => r.json())
+      .then((data) => setData(data));
+  }, []);
+
+  function DisplayTransactions({ data }) {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>DATE</th>
+            <th>DESCRIPTION</th>
+            <th>CATEGORY</th>
+            <th>AMOUNT</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.date}</td>
+                <td>{item.description}</td>
+                <td>{item.category}</td>
+                <td>{item.amount}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
+  function AddNewTransaction() {
+    return (
+      <form className="form">
+       <label>DATE<input type="text" value="date" /></label> 
+       <label>DESCRIPTION<input type="text" value="description"/></label> 
+       <label>CATEGORY<input type="text" value="category" /></label> 
+       <label>AMOUNT<input type="text" value="amount" /></label> 
+      </form>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <DisplayTransactions data={data} /> */}
+      <AddNewTransaction />
     </div>
   );
 }
